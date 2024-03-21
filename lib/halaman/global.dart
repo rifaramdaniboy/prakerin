fd9a25b330a892cd5c06bedf1fdb138a98a02260
rifaramdaniboy <rifaramdani42@gmail.com>
2024-03-21 22:48:05 +0700

    first commit

--- lib/halaman/global.dart
+++ lib/halaman/global.dart
@@ -9,29 +9,71 @@ class PageGlobal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final DataGlobal _getGlobal = new DataGlobal();
-    return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: AppColors.mainColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25),
+    return ListView(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.mainColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                ),
+              ),
+              padding: EdgeInsets.only(top: 20, bottom: 30),
+              child: Stack(
+                children: <Widget>[
+                  Image.asset("assets/images/virus2.png"),
+                  _homeHeader(),
+                ],
               ),
             ),
-            padding: EdgeInsets.only(top: 20, bottom: 30),
-            child: Stack(
-              children: <Widget>[
-                Image.asset("assets/images/virus2.png"),
-                _homeHeader(),
-              ],
+            SizedBox(height: 20.0),
+            Center(
+                child: Text(
+              'Data Kasus Corona Berdasarkan Global',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            )),
+            Padding(
+              padding: EdgeInsets.all(6.0),
             ),
-          ),
-        ],
-      ),
+            FutureBuilder(
+              future: _getGlobal.getDataGlobal(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<CovidGlobal>> snapshot) {
+                if (snapshot.hasData) {
+                  List<CovidGlobal> dataGlobal = snapshot.data;
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      columns: [
+                        DataColumn(label: Text('Negara')),
+                        DataColumn(label: Text('Positif')),
+                        DataColumn(label: Text('Sembuh')),
+                        DataColumn(label: Text('Meninggal')),
+                      ],
+                      rows: dataGlobal
+                          .map((CovidGlobal dataGlobal) => DataRow(cells: [
+                                DataCell(Text(dataGlobal.countryRegion)),
+                                DataCell(Text(dataGlobal.confirmed.toString())),
+                                DataCell(Text(dataGlobal.recovered.toString())),
+                                DataCell(Text(dataGlobal.deaths.toString())),
+                              ]))
+                          .toList(),
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ],
+        )
+      ],
     );
   }
 
@@ -86,8 +128,8 @@ class PageGlobal extends StatelessWidget {
           ),
         ),
         Container(
-          width: 50,
-          height: 50,
+          width: 10,
+          height: 10,
           margin: EdgeInsets.all(16),
         ),
       ],
--- lib/halaman/provinsi.dart
+++ lib/halaman/provinsi.dart
@@ -10,72 +10,71 @@ class PageProvinsi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final DataIndo _getIndo = new DataIndo();
-    return Scaffold(
-      backgroundColor: AppColors.backgroundColor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: AppColors.mainColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25),
+    return ListView(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.mainColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                ),
+              ),
+              padding: EdgeInsets.only(top: 20, bottom: 30),
+              child: Stack(
+                children: <Widget>[
+                  Image.asset("assets/images/virus2.png"),
+                  _homeHeader(),
+                ],
               ),
             ),
-            padding: EdgeInsets.only(top: 25, bottom: 30),
-            child: Stack(
-              children: <Widget>[
-                Image.asset("assets/images/virus2.png"),
-                _homeHeader(),
-              ],
+            SizedBox(height: 20.0),
+            Center(
+                child: Text(
+              'Data Kasus Corona Berdasarkan Provinsi',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            )),
+            Padding(
+              padding: EdgeInsets.all(6.0),
             ),
-          ),
-          SizedBox(height: 20.0),
-          Center(
-              child: Text(
-            'Data Kasus Corona Berdasarkan Provinsi',
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          )),
-          Padding(
-            padding: EdgeInsets.all(6.0),
-          ),
-          FutureBuilder(
-            future: _getIndo.getDataIndo(),
-            builder: (BuildContext context,
-                AsyncSnapshot<List<CovidProvinsi>> snapshot) {
-              if (snapshot.hasData) {
-                List<CovidProvinsi> dataIndo = snapshot.data;
-                return SingleChildScrollView(
-                  child: DataTable(
-                    columns: [
-                      DataColumn(label: Text('Provinsi')),
-                      DataColumn(label: Text('Positif')),
-                      DataColumn(label: Text('Sembuh')),
-                      DataColumn(label: Text('Meninggal')),
-                    ],
-                    rows: dataIndo
-                        .map((CovidProvinsi dataIndo) => DataRow(cells: [
-                              DataCell(Text(dataIndo.provinsi)),
-                              DataCell(Text(dataIndo.kasus_posi.toString())),
-                              DataCell(Text(dataIndo.kasus_sem.toString())),
-                              DataCell(Text(dataIndo.kasus_meni.toString())),
-                            ]))
-                        .toList(),
-                  ),
-                );
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-          ),
-        ],
-      ),
+            FutureBuilder(
+              future: _getIndo.getDataIndo(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<CovidProvinsi>> snapshot) {
+                if (snapshot.hasData) {
+                  List<CovidProvinsi> dataIndo = snapshot.data;
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      columns: [
+                        DataColumn(label: Text('Provinsi')),
+                        DataColumn(label: Text('Positif')),
+                        DataColumn(label: Text('Sembuh')),
+                        DataColumn(label: Text('Meninggal')),
+                      ],
+                      rows: dataIndo
+                          .map((CovidProvinsi dataIndo) => DataRow(cells: [
+                                DataCell(Text(dataIndo.provinsi)),
+                                DataCell(Text(dataIndo.kasus_posi.toString())),
+                                DataCell(Text(dataIndo.kasus_sem.toString())),
+                                DataCell(Text(dataIndo.kasus_meni.toString())),
+                              ]))
+                          .toList(),
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ],
+        )
+      ],
     );
   }
 
@@ -129,9 +128,9 @@ class PageProvinsi extends StatelessWidget {
           style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
         ),
         Container(
-          width: 50,
-          height: 50,
-          margin: EdgeInsets.all(10),
+          width: 10,
+          height: 10,
+          margin: EdgeInsets.all(16),
         ),
       ],
     );
